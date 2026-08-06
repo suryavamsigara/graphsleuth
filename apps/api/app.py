248f39ce3c386c1c05e928c626ff18ef3e1c0af6
Surya Vamsi <suryavamsigara2@gmail.com>
2026-08-06 20:54:35 +0000

    Update CORS allowed origins to include Amplify deployment URL

--- apps/api/app.py
+++ apps/api/app.py
@@ -26,7 +26,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000"],
+    allow_origins=["http://localhost:5173", "http://localhost:3000", "https://master.d20qrm73fi6zbj.amplifyapp.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
