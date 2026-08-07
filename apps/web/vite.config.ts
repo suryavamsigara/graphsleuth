7908f43e361700967dc84cf690485b01c592cf13
Surya Vamsi <suryavamsigara2@gmail.com>
2026-08-07 04:53:35 +0000

    Update dependencies

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from '@tailwindcss/vite'
 import path from "path";
 import { fileURLToPath } from "url";
 
@@ -9,8 +8,7 @@ const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [
-    react(),
-    tailwindcss(),
+    react()
   ],
   resolve: {
     alias: {
